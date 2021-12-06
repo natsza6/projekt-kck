b71b2f99a2cc79aade2d985654ae11200d14d4e5
natsza6 <83343731+natsza6@users.noreply.github.com>
2021-12-06 13:54:40 +0100

    dostosowanie czcionki

--- analiza_sygnalow.py
+++ analiza_sygnalow.py
@@ -3,19 +3,27 @@ from tkinter import *
 import tkinter as tk
 import time
 import random
+import tkinter.font as font
 
 window = tk.Tk()
 window.title("Alfabet")
-window.geometry('1200x1000')
+window.geometry('1000x700')
 window.config(bg = '#FFFFFF')
 
+myFont1 = font.Font(family='Helvetica', size=50, weight='bold')
+myFont2 = font.Font(family='Helvetica', size=30, weight='normal')
 
+tytul = tk.Label(window, text = "Instrukcja")
+instrukcja = tk.Label(window, text = "Przed Tobą znajdzię się cały polski alfabet. \n Litery będa pojawiać się po kolei w odstępie czasowym jednej sekundy. \n Kliknij przycisk, aby rozpocząć.")
 
-tytul = tk.Label(window, text = "Instrukcja", size = 30)
-instrukcja = tk.Label(window, text = "Przed Tobą znajdzię się cały polski alfabet. Litery będa pojawiać się po kolei w odstępie czasowym jednej sekundy. Kliknij przycisk, aby rozpocząć.", size = 20)
+tytul['font'] = myFont1
+instrukcja['font'] = myFont2
 
 b1 = Button(window, text = "Rozpocznij")
 b2 = Button(window, text = "Wyjdź")
+
+b1['font'] = myFont2
+b2['font'] = myFont2
 
 tytul.pack()
 instrukcja.pack()
